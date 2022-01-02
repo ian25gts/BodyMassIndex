238f9d812ae3cb7fcc73bc550b932b03bea6d40b
Ivanov Alexey <ian25@rambler.ru>
2022-01-02 21:03:27 +0300

    second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate (int mass){
-        double growth = 1.72;
+    public double calculate (int mass, double growth){
         double calcGrowth = growth * growth;
         double calcBmi = mass / calcGrowth;
         return calcBmi;
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int mass = 74;
-        double bmi = service.calculate(mass);
+        double growth = 1.72;
+        double bmi = service.calculate(mass, growth);
         String result = String.format("%.2f",bmi);
         System.out.println(result);
     }
